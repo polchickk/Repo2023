7ef76759f7cfc259d91229c5f85da04fbf4171db
polina <p.l.pavlova@urfu.me>
2023-02-16 17:36:27 +0500

    фильстр с изменением яркости

--- PhotoEnhancer/PhotoEnhancer/Form1.Designer.cs
+++ PhotoEnhancer/PhotoEnhancer/Form1.Designer.cs
@@ -31,33 +31,64 @@ namespace PhotoEnhancer
         {
             this.originalPictureBox = new System.Windows.Forms.PictureBox();
             this.resultPictureBox = new System.Windows.Forms.PictureBox();
+            this.filtersComboBox = new System.Windows.Forms.ComboBox();
+            this.applyButton = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.originalPictureBox)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.resultPictureBox)).BeginInit();
             this.SuspendLayout();
             // 
             // originalPictureBox
             // 
-            this.originalPictureBox.Location = new System.Drawing.Point(52, 30);
+            this.originalPictureBox.Location = new System.Drawing.Point(405, 30);
             this.originalPictureBox.Name = "originalPictureBox";
             this.originalPictureBox.Size = new System.Drawing.Size(523, 710);
+            this.originalPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.originalPictureBox.TabIndex = 0;
             this.originalPictureBox.TabStop = false;
             // 
             // resultPictureBox
             // 
-            this.resultPictureBox.Location = new System.Drawing.Point(625, 207);
+            this.resultPictureBox.Location = new System.Drawing.Point(947, 30);
             this.resultPictureBox.Name = "resultPictureBox";
             this.resultPictureBox.Size = new System.Drawing.Size(523, 710);
+            this.resultPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.resultPictureBox.TabIndex = 1;
             this.resultPictureBox.TabStop = false;
+            // 
+            // filtersComboBox
+            // 
+            this.filtersComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.filtersComboBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.filtersComboBox.FormattingEnabled = true;
+            this.filtersComboBox.Location = new System.Drawing.Point(12, 30);
+            this.filtersComboBox.Name = "filtersComboBox";
+            this.filtersComboBox.Size = new System.Drawing.Size(373, 28);
+            this.filtersComboBox.TabIndex = 2;
+            this.filtersComboBox.SelectedIndexChanged += new System.EventHandler(this.filtersComboBox_SelectedIndexChanged);
+            // 
+            // applyButton
+            // 
+            this.applyButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.applyButton.Location = new System.Drawing.Point(98, 709);
+            this.applyButton.Name = "applyButton";
+            this.applyButton.Size = new System.Drawing.Size(143, 36);
+            this.applyButton.TabIndex = 3;
+            this.applyButton.Text = "применить";
+            this.applyButton.UseVisualStyleBackColor = true;
+            this.applyButton.Visible = false;
+            this.applyButton.Click += new System.EventHandler(this.applyButton_Click);
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1082, 1055);
+            this.ClientSize = new System.Drawing.Size(1482, 1055);
+            this.Controls.Add(this.applyButton);
+            this.Controls.Add(this.filtersComboBox);
             this.Controls.Add(this.resultPictureBox);
             this.Controls.Add(this.originalPictureBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
             this.Name = "MainForm";
             this.Text = "PhotoEnhancer";
             ((System.ComponentModel.ISupportInitialize)(this.originalPictureBox)).EndInit();
@@ -70,6 +101,8 @@ namespace PhotoEnhancer
 
         private System.Windows.Forms.PictureBox originalPictureBox;
         private System.Windows.Forms.PictureBox resultPictureBox;
+        private System.Windows.Forms.ComboBox filtersComboBox;
+        private System.Windows.Forms.Button applyButton;
     }
 }
 
--- PhotoEnhancer/PhotoEnhancer/Form1.cs
+++ PhotoEnhancer/PhotoEnhancer/Form1.cs
@@ -15,9 +15,91 @@ namespace PhotoEnhancer
         Bitmap originalBmp;
         Bitmap resultBmp;
 
+        Panel parametersPanel;
+
         public MainForm()
         {
             InitializeComponent();
+
+            originalBmp=(Bitmap)Image.FromFile("cat.jpg");
+            originalPictureBox.Image = originalBmp;
+
+            filtersComboBox.Items.Add("Осветление/Затемнение");
+
+        }
+
+        private void filtersComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            applyButton.Visible = true;
+
+            if (parametersPanel != null)
+                this.Controls.Remove(parametersPanel);
+
+            parametersPanel = new Panel();
+
+            parametersPanel.Left = filtersComboBox.Left;
+            parametersPanel.Top = filtersComboBox.Bottom + 10;
+            parametersPanel.Width = filtersComboBox.Width;
+            parametersPanel.Height = applyButton.Top - filtersComboBox.Bottom - 20;
+
+            this.Controls.Add(parametersPanel);
+
+            if(filtersComboBox.SelectedItem.ToString()== "Осветление/Затемнение")
+            {
+                var label = new Label();
+                label.Left = 0;
+                label.Top = 0;
+                label.Width = parametersPanel.Width - 50;
+                label.Height = 28;
+                label.Text = "Коэффициент";
+                label.Font = new Font(label.Font.FontFamily,10);
+                parametersPanel.Controls.Add(label);
+
+                var inputBox = new NumericUpDown();
+                inputBox.Left = label.Right;
+                inputBox.Top = label.Top;
+                inputBox.Width = parametersPanel.Width-label.Width;
+                inputBox.Height = label.Height;
+                inputBox.Font = new Font(inputBox.Font.FontFamily, 10);
+                inputBox.Maximum = 10;
+                inputBox.Minimum = 0;
+                inputBox.Increment =(decimal)0.05;
+                inputBox.DecimalPlaces = 2;
+                inputBox.Value = 1;
+                inputBox.Name = "coefficient";
+                parametersPanel.Controls.Add(inputBox);
+            }
+        }
+
+        private void applyButton_Click(object sender, EventArgs e)
+        {
+            var newBmp = new Bitmap(originalBmp.Width,originalBmp.Height);
+
+            if (filtersComboBox.SelectedItem.ToString() == "Осветление/Затемнение")
+            {
+                var k = (double)((NumericUpDown)parametersPanel.Controls["coefficient"]).Value;
+                for(var x=0;x< originalBmp.Width;x++)
+                    for (var y = 0; y < originalBmp.Height; y++)
+                    {
+                        var pixelColor = originalBmp.GetPixel(x, y);
+                        
+                        var newR = (int)(pixelColor.R * k);
+                        if (newR > 255)
+                            newR = 255;
+
+                        var newG = (int)(pixelColor.G * k);
+                        if (newG > 255)
+                            newG = 255;
+
+                        var newB = (int)(pixelColor.B * k);
+                        if (newB > 255)
+                            newB = 255;
+
+                        newBmp.SetPixel(x, y, Color.FromArgb(newR, newG, newB));
+                    }
+            }
+            resultBmp = newBmp;
+            resultPictureBox.Image = resultBmp;
         }
     }
 }
