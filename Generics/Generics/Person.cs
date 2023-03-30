e7c19dfe37fb640b516a51a28f3e90e35f35bfb9
polina <p.l.pavlova@urfu.me>
2023-03-30 17:17:24 +0500

    дженерики

---
+++ Generics/Generics/Person.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    internal class Person<T>
+    {
+        public string Name { get; set; }    
+
+        public T Description { get; set; }
+    }
+}
---
+++ Generics/Generics/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace Generics
+{
+    internal static class Program
+    {
+        static void Main(string[] args) 
+        { 
+            var integers = new[] {1,2,3,4,5};
+            var doubles = new[] { 1.5, -2.0, 3 };
+
+            PrintNumbers<int>(integers);
+            Console.WriteLine( );
+            PrintNumbers<double>(doubles);
+
+            var john = new Person<string> { Name = "John", Description = "Text" };
+            var kate = new Person<int> { Name = "Kate", Description = 1 };
+            Console.ReadKey();
+        }
+
+        static void PrintNumbers<T>(T[] values)
+        {
+            foreach(var value in values) 
+                Console.Write($"{value} ");
+        }
+    }
+}
